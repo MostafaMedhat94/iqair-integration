ea39570d5c0fe50de9a42705d1705ddda0d4e2a0
Mostafa Medhat <mostafa.medhat@dstny.com>
2024-01-07 11:54:14 +0200

    Read Service TCP Port From The Environment

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from './logger/logger.service';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new Logger(),
   });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port || 3000);
 }
 bootstrap();
