2c51641f2cefb379aa274f98f6232b83fc4393e1
Mostafa Medhat <mostafa.medhat@dstny.com>
2024-01-07 11:53:00 +0200

    Add Full Relative Path To Fix Import Issue in E2E Tests

--- src/air-data/air-data.module.ts
+++ src/air-data/air-data.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AirDataService } from './air-data.service';
 import { AirDataController } from './air-data.controller';
 import { CityAirQuality, CityAirQualitySchema } from './schemas';
-import { Logger } from 'src/logger/logger.service';
+import { Logger } from '../logger/logger.service';
 
 @Module({
   imports: [
