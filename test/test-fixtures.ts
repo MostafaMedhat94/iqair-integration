4ef61d057377f65d53416f2e644e0d52954a0239
Mostafa Medhat <mostafa.medhat@dstny.com>
2024-01-09 11:33:25 +0200

    Refactor E2E Tests

--- test/test-fixtures.ts
+++ test/test-fixtures.ts
@@ -2,11 +2,11 @@ export const TEST_FIXTURES = {
   airDataResposne: {
     Result: {
       Pollution: {
-        ts: '2024-01-02T22:00:00.000Z',
-        aqius: 27,
-        mainus: 'o3',
-        aqicn: 21,
-        maincn: 'o3',
+        ts: '2024-01-08T06:00:00.000Z',
+        aqius: 71,
+        mainus: 'p2',
+        aqicn: 31,
+        maincn: 'p2',
       },
     },
   },
